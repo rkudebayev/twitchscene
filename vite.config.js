459a34a9dd35d13c070b77d79317938e2310a959
Rassul Kudebayev <rasulito92@192.168.10.8>
2025-08-24 16:57:20 +0500

    root fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'src',       // если исходники в src
+  root: '.',       // если исходники в src
   build: {
     outDir: '../dist' // собираем в dist на уровне корня проекта
   }

e1672e4d1271f1a438619858fecd21d0ed9bdabc
Rassul Kudebayev <rasulito92@192.168.10.8>
2025-08-24 18:04:11 +0500

    rrr

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// если index.html в корне, main.jsx в src
 export default defineConfig({
   plugins: [react()],
-  root: '.',            // исходники в src
-  base: './',             // относительные пути к ресурсам
+  root: '.',              // корень проекта
+  base: './',             // очень важно для относительных путей в билде
   build: {
-    outDir: '../dist',    // собрать в dist на уровне корня проекта
-    emptyOutDir: true
+    outDir: 'dist',       // куда собирать файлы
+    emptyOutDir: true,    // очищать dist перед билдом
+    rollupOptions: {
+      input: 'index.html' // указываем Vite явно точку входа
+    }
   }
 })
